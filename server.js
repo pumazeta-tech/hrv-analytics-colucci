e59eaf0f421579017b85596e45c977c131e1fd15
pumazeta-tech <pumazeta@gmail.com>
2025-11-04 10:18:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
-  },
-  debug: true, // Aggiungi questo
-  logger: true  // E questo
+  }
 });
 
 app.post('/send-report', async (req, res) => {
